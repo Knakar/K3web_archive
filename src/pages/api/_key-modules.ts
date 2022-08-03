27b6db15773ba06fec55e92245dbbe7eb27b5c61
knakar <knakar@github.com>
2022-08-03 21:02:40 +0900

    feat: リクエストのcookieのtokenと秘密鍵の一致を検証

--- src/pages/api/_key-modules.ts
+++ src/pages/api/_key-modules.ts
@@ -1,5 +1,8 @@
 import {randomBytes, createHash} from 'crypto'
+import { NextRequest } from 'next/server';
+import {PrismaClient} from 'prisma/prisma-client/scripts/default-index';
 
+const prisma = new PrismaClient();
 type KeyPair  = {
   public: string,
   private: string
@@ -18,3 +21,15 @@ function certificateKeys(pubKey: string, pbKey: string): boolean {
   return hashedKey === pbKey;
 }
 
+export async function authenticateRequest(request: NextRequest): Promise<boolean> {
+  //Todo: cors certification
+  const token: string | undefined = request.cookies.get('token');
+  const userId : string | undefined = request.cookies.get('id');
+  if(!token&&userId) return false;
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userId
+    }
+  });
+  return certificateKeys(token as string, user.key as string);
+}
